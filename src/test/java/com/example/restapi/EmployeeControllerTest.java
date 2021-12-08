d2799343de2689a2494009f9df69c00def1612d3
Gloria Chan <changl3@oocl.com>
2021-12-08 11:59:16 +0800

    [RestApiPractice2][Gloria] feat: testcase: should_return_employee_list_when_perform_getByPage_given_employees()

--- src/test/java/com/example/restapi/EmployeeControllerTest.java
+++ src/test/java/com/example/restapi/EmployeeControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.MultiValueMap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -86,5 +87,35 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$[0].age").value("19"))
                 .andExpect(jsonPath("$[0].gender").value("Male"))
                 .andExpect(jsonPath("$[0].salary").value("1920213"));
+    }
+
+    @Test
+    void should_return_employee_list_when_perform_getByPage_given_employees() throws Exception {
+        //given
+        Employee employee1 = new Employee(1, "Marcus", 19, "Male", 1920213);
+        Employee employee2 = new Employee(2, "Gloria", 22, "Female", 1000000);
+        Employee employee3 = new Employee(3, "Linne", 22, "Female", 1000000);
+
+        employeeRepository.create(employee1);
+        employeeRepository.create(employee2);
+        employeeRepository.create(employee3);
+        //when
+
+        //then
+        String pageNum = "1";
+        String pageSize = "2";
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees?page="+pageNum+"&pageSize="+pageSize))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("Marcus"))
+                .andExpect(jsonPath("$[0].age").value("19"))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].salary").value("1920213"))
+                .andExpect(jsonPath("$[1].id").isNumber())
+                .andExpect(jsonPath("$[1].name").value("Gloria"))
+                .andExpect(jsonPath("$[1].age").value("22"))
+                .andExpect(jsonPath("$[1].gender").value("Female"))
+                .andExpect(jsonPath("$[1].salary").value("1000000"));
     }
 }
