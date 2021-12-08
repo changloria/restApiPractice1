da90f28333ea5c73af4a12477fe4babc5b3b1781
Marcus Ho <marcus.ho@oocl.com>
2021-12-08 11:35:26 +0800

    [RestApiPractice2][Marcus] feat: test case get all

--- src/test/java/com/example/restapi/EmployeeControllerTest.java
+++ src/test/java/com/example/restapi/EmployeeControllerTest.java
@@ -21,5 +21,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 public class EmployeeControllerTest {
-    
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void cleanRepository() {
+        employeeRepository.cleaarAll();
+    }
+
+
+    @Test
+    void should_get_all_employees_when_perform_get_given_employees() throws Exception {
+        //given
+        Employee employee = new Employee(1, "Marcus", 19, "Male", 1920213);
+        employeeRepository.create(employee);
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("Marcus"))
+                .andExpect(jsonPath("$[0].age").value("19"))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].salary").value("1920213"));
+    }
 }
