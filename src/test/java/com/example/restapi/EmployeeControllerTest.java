92840ef9865349e85012a4087f2e1f7cb2c24f19
Marcus Ho <marcus.ho@oocl.com>
2021-12-08 11:49:15 +0800

    [RestApiPractice2][Marcus] feat: testcase: should_get_all_employees_when_perform_getByGender_given_employees_and_gender()

--- src/test/java/com/example/restapi/EmployeeControllerTest.java
+++ src/test/java/com/example/restapi/EmployeeControllerTest.java
@@ -36,12 +36,15 @@ public class EmployeeControllerTest {
     void should_get_all_employees_when_perform_get_given_employees() throws Exception {
         //given
         Employee employee = new Employee(1, "Marcus", 19, "Male", 1920213);
+        Employee employee2 = new Employee(1, "Gloria", 22, "Female", 1000000);
         employeeRepository.create(employee);
+        employeeRepository.create(employee2);
+
         //when
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id").isNumber())
                 .andExpect(jsonPath("$[0].name").value("Marcus"))
                 .andExpect(jsonPath("$[0].age").value("19"))
@@ -63,5 +66,25 @@ public class EmployeeControllerTest {
                 .andExpect(jsonPath("$.age").value("22"))
                 .andExpect(jsonPath("$.gender").value("female"))
                 .andExpect(jsonPath("$.salary").value("1000000"));
+    }
+
+    @Test
+    void should_get_all_employees_when_perform_getByGender_given_employees_and_gender() throws Exception {
+        //given
+        Employee employee = new Employee(1, "Marcus", 19, "Male", 1920213);
+        Employee employee2 = new Employee(1, "Gloria", 22, "Female", 1000000);
+
+        employeeRepository.create(employee);
+        employeeRepository.create(employee2);
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees").param("gender", employee.getGender()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("Marcus"))
+                .andExpect(jsonPath("$[0].age").value("19"))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].salary").value("1920213"));
     }
 }
